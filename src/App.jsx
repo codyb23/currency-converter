00ef9a76c71c3d42828f8c102562b3d84c64bdd7
Cody Banks <codydouglasbanks@gmail.com>
2020-07-01 14:54:45 -0400

    moved from static data to an API fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import HelloWorld from './components/HelloWorld'
 
 function App() {
-  return <HelloWorld />
+  const [currencyDetails, setCurrencyDetails] = useState({})
+  const [currencyRates, setCurrencyRates] = useState({})
+
+  const loadCurrencyDetailsFromApi = () => {
+    const url = 'https://api.ratesapi.io/api/latest?base=USD'
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((apiData) => {
+        setCurrencyDetails(apiData)
+        setCurrencyRates(apiData.rates)
+      })
+  }
+
+  useEffect(loadCurrencyDetailsFromApi, [])
+
+  return (
+    <main>
+      <header class="jumbotron jumbotron-fluid">
+        <div class="container">
+          <h1 class="display-4">Currency Rates</h1>
+          <p class="lead">{currencyDetails.base}</p>
+        </div>
+      </header>
+      <body>
+        <div class="input-group">
+          <div class="input-group-prepend">
+            <span class="input-group-text">
+              Base Currency Value: {currencyDetails.base}
+            </span>
+          </div>
+          <textarea class="form-control" aria-label="With textarea"></textarea>
+        </div>
+        <section>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">
+              <h5>GBP</h5> {currencyRates.GBP}
+            </li>
+            <li class="list-group-item">
+              <h5>HKD</h5> {currencyRates.HKD}
+            </li>
+            <li class="list-group-item">Morbi leo risus</li>
+            <li class="list-group-item">Porta ac consectetur ac</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+        </section>
+      </body>
+    </main>
+  )
 }
 
 export default App
